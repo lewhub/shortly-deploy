bf077ee30866a845151f54af2c56bf8dad030eba
lewhub <bracey.lewis@gmail.com>
2017-06-10 10:44:23 -0700

    testing grunt change

--- server.js
+++ server.js
@@ -7,3 +7,4 @@ app.listen(port);
 console.log('Server now listening on port ' + port);
 console.log('testing change on local to live')
 console.log('testing live change two')
+console.log('this is another test')
